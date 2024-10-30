1f16d57c1753323f47fb69c7212e9048b020af96
Tabish Raza <ts764raza@gmail.com>
2024-10-30 19:54:33 +0500

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { IoHeartOutline } from "react-icons/io5";
+
+
+
+
+import { CiSearch } from "react-icons/ci";
+
+const Header = () => {
+  
+  return (
+    <header >
+      <div className='flex items-center justify-between mx-[14rem]  my-4'>
+        <div className='flex items-center'>
+          <img src='/Logo.png' className='mr-[4rem]' ></img>
+          <div className='relative flex items-center'>
+          <CiSearch style={{ width: '26px', height: '26px'}} className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          <input
+            className='bg-gray-100 h-12 text-sm font-[400]  pl-10 w-80 rounded-lg'
+            placeholder='Search'
+            type="search"
+            aria-label="Search"
+          />
+          </div>
+        </div>
+        <nav className='flex font-semibold text-gray-400 items-center gap-9'>
+          <a className='text-black' href="#">Home</a>
+          <a  className='text2' href="#">About</a>
+          <a className='text2' href="#">Contact Us</a>
+          <a className='text2' href="#">Blog</a>
+        </nav>
+        <div className='flex items-center gap-5 mr-[5rem]'>
+          <IoHeartOutline className='hover:fill-red-600' style={{ width: '26px', height: '26px'}} />
+          <img src='/Cart.png' style={{ width: '26px', height: '26px'}}  />
+          <img src='/User.png'  style={{ width: '26px', height: '26px'}}  />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/components/Hero.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className='bg  flex text-white'>
+        <div className='flex  justify-between w-full mx-[10rem] '>
+        <div className='flex flex-col mt-[12rem]  gap-1'>
+            <p className='font-medium text text-2xl'>Pro.Beyond.</p>
+            <h1 className='text-8xl font-[100] mb-2'>IPhone 14 <span className='font-[500]'>Pro</span></h1>
+            <h2 className='mb-2 text-xl text'>Created to change everything for the better. For everyone</h2>
+            <div>
+            <button className='border py-4 mt-2 px-[3rem] rounded '>Shop Now</button>
+            </div>
+           
+        </div>
+        <div className=' mb-5 mt-[5rem]'>
+                <img className='w-[25rem]' src='/12121.PNG'></img>
+            </div>
+            </div>
+    </div>
+  )
+}
+
+export default Hero
